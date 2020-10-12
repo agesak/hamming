1989d054028a04cd59b2b747fec755032041aa76
Kareha Agesa <kmaagesa@gmail.com>
2020-10-11 23:06:36 -0700

    pushing

---
+++ hamming.rb
@@ -0,0 +1,31 @@
+=begin
+Write your code for the 'Hamming' exercise in this file. Make the tests in
+`hamming_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/hamming` directory.
+=end
+
+class Hamming
+
+  def self.compute(string1, string2)
+
+    raise ArgumentError.new("Strings must be same length") if string1.length != string2.length
+
+    # array = string1 + string2
+    # min_length = array.min_by{|string| string.length}
+
+    differences = 0
+    # randomly picked length of 1st string
+    string1.length.times do |i|
+      if string1[i] != string2[i]
+        differences += 1
+      end
+    end
+    return differences
+  end
+
+
+end
+
+
